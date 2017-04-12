d9a4b2ae08d51a497f8502c9e23398428f509e62
mongkok <dani.pyc+github@gmail.com>
2017-04-13 02:10:04 +0700

    :racehorse:

---
+++ 9.6/init-user-db.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+source /docker-entrypoint-initdb.d/functional
+
+initialize_database
+configure_recovery
+create_database
+create_replication_user
+
+set_postgresql_param "listen_addresses" "*" quiet
+
+case $REPLICATION_MODE in
+  slave|snapshot|backup)
+    gosu postgres pg_ctl -D "$PGDATA" -w start >/dev/null
+    cp /dev/null /docker-entrypoint-initdb.d/postgis.sh
+    ;;
+esac
